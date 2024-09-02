f199f05a5016af204d2a5166b7363f4e085e89e1
Rogerio Panizza <67240761+RogerioPanizza@users.noreply.github.com>
2024-09-02 07:55:15 -0300

    Add files via upload

---
+++ main_pessoa.py
@@ -0,0 +1,15 @@
+# Microatividade 02
+
+import Pessoa
+
+
+self.nome = 'João' #(ref. ao atributo nome)
+self.dataNascimento = 2000-01-01 #(ref. ao atributo dataNascimento)
+self.cpf = 000.111.222-33 #(ref. ao atributo cpf)
+self.rg = 15975388-1 # (ref. ao atributo rg
+
+attrs = vars(NOME-DO-OBJETO-CRIADO)
+
+print('Instancia da classe Pessoa: ')
+
+print(', '.join("%s: %s" % item for item in attrs.items()))
